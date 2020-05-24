c131b8aad5a5306f692a82e92d6f778b2971fe87
Alan Sha Salim <alanthegamer@gmail.com>
2020-05-24 19:19:15 +0400

    Added header and banner, angular components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './view-frontend/header/header.component';
+import { BannerComponent } from './view-frontend/banner/banner.component';
+import { WelcomeComponent } from './view-frontend/welcome/welcome.component';
+import { WidgetsComponent } from './view-frontend/widgets/widgets.component';
+import { FooterComponent } from './view-frontend/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    WelcomeComponent,
+    WidgetsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
